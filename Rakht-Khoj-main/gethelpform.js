f0e6ad8f6c7bb48594c91da60e8207d3cd74586d
PosInfinity <121788666+PosInfinity@users.noreply.github.com>
2023-03-24 22:34:06 +0530

    Firebase connectivity of Help.html

---
+++ Rakht-Khoj-main/gethelpform.js
@@ -0,0 +1,43 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBLd4WyIMw6m-oN_MiKleYHRO36MagP3lg",
+    authDomain: "get-help-form.firebaseapp.com",
+    databaseURL: "https://get-help-form-default-rtdb.firebaseio.com",
+    projectId: "get-help-form",
+    storageBucket: "get-help-form.appspot.com",
+    messagingSenderId: "273951338669",
+    appId: "1:273951338669:web:cd9b77999039cc5369d624"
+  };
+  // initialize firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //reference your database
+  var get_help_formDB = firebase.database().ref("GetHelp");
+
+  document.getElementById("gethelpform").addEventListener('submit',submitForm);
+
+  function submitForm(e){
+    e.preventDefault();
+    var name = getElementVal("name");
+    var email = getElementVal("email");
+    var phone = getElementVal("phone");
+    var message = getElementVal("msg");
+
+    saveMessages(name,email,phone,msg);
+
+  }
+
+  const saveMessages =(name,email,phone,msg) =>{
+    var newGetHelpForm = get_help_formDB.push();
+    newGetHelpForm.set({
+      name : name,
+      email : email,
+      phone : phone,
+      msg : msg,
+    });
+    
+  }
+
+  const getElementVal = (id) =>{
+    return document.getElementById(id).value;
+  };
