4c1fd1677830c1c1b1282e17b126f5900c6387b6
dev02chandan <dev02chandan@gmail.com>
2023-04-03 08:22:57 +0530

    Logo changed, removed blood buddy text,etc.

--- Rakht-Khoj-main/login.js
+++ Rakht-Khoj-main/login.js
@@ -1,64 +1,66 @@
-import { getDatabase, set, ref ,push, child, onValue} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  push,
+  child,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 import database from "./firebasecred.js";
 
-
-
 console.log("js reach");
-login_submit.addEventListener('click',(e) => {
-    console.log("submit reach");
-    e.preventDefault();
-    var username = getElementVal("username_login");
-    var password = getElementVal("password_login");
-    matchingreq(username,password);
-    
+login_submit.addEventListener("click", (e) => {
+  console.log("submit reach");
+  e.preventDefault();
+  var username = getElementVal("username_login");
+  var password = getElementVal("password_login");
+  matchingreq(username, password);
 });
 
-
-  
-  const getElementVal = (id) =>{
-    return document.getElementById(id).value;
-  };
-
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
 
 //   var el;
-var flag=false;
-  const matchingreq = (username,password)=>{
-    const dbRef = ref(database, 'RegisterForm/');
-      onValue(dbRef, (snapshot) => {
-        snapshot.forEach((childSnapshot) => {
+var flag = false;
+const matchingreq = (username, password) => {
+  const dbRef = ref(database, "RegisterForm/");
+  onValue(
+    dbRef,
+    (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
         const childKey = childSnapshot.key;
         const childData = childSnapshot.val();
-         
+
         // console.log(typeof childData.username);
         // console.log(childData.username);
         // console.log(childData.password);
         // console.log((childData.username == username));
         // console.log((childData.password == password));
-        if ((childData.username == username) && (childData.password==password)){
-        
+        if (childData.username == username && childData.password == password) {
           console.log("match");
-        //   flag = true;
-        //   if(flag){return;}
-        window.location.href = "index.html";
-          
+          //   flag = true;
+          //   if(flag){return;}
+          window.location.href = "index.html";
+        } else {
+          var el = document.getElementById("response");
+          if (el) {
+            el.innerHTML = "Wrong Credentials Entered!!! Re-enter Details";
+          } else {
+            console.log("element not found");
+          }
         }
-        else{
-            var el = document.getElementById("response");
-            if (el) {
-                el.innerHTML = "Wrong Credentials Entered!!! Re-enter Details";
-            } else {
-                console.log('element not found');
-            }
-        }
-        });
-      }, {
-          onlyOnce: true
-    });
-  
-      if(flag==false){
-        console.log("non-match");
-        console.log(flag);
-      }
+      });
+    },
+    {
+      onlyOnce: true,
+    }
+  );
 
+  if (flag == false) {
+    console.log("non-match");
+    console.log(flag);
   }
-  
+};
+
+localStorage.setItem("myVariable", "hello");
